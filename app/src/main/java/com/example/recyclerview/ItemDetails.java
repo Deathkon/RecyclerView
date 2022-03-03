776fffbb44fdcf97b10766c270550b1d6e0ffb70
Deathkon <shyakasmith@gmail.com>
2022-03-03 09:35:43 +0200

    commit

--- app/src/main/java/com/example/recyclerview/ItemDetails.java
+++ app/src/main/java/com/example/recyclerview/ItemDetails.java
@@ -3,22 +3,27 @@ package com.example.recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 public class ItemDetails extends AppCompatActivity {
     ImageView imageView;
-    TextView name,price,tvDescription;
-     @Override
+    TextView name,price,description;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_details);
-        imageView = findViewbyId(R.id.itemImageD);
-        name = findViewbyId(R.id.itemNameD);
-        price = findViewbyId(R.id.itemPriceD);
-        description = findViewbyId(R.id.itemDescrD);
-        Intent intent = getIntent();
+        imageView = findViewById(R.id.itemImageD);
+        name = findViewById(R.id.itemNameD);
+        price = findViewById(R.id.itemPriceD);
+        description = findViewById(R.id.itemDescrD);
 
+        Intent intent = getIntent();
         imageView.setImageResource(intent.getIntExtra("image", 0));
-        name.setText(intent.getStringExtra("Name", 0));
+        name.setText(intent.getStringExtra("Name"));
         price.setText(" "+intent.getFloatExtra("Price", 0));
-        description.setText(intent.getStringExtra("Description", 0));
-        }
+        description.setText(intent.getStringExtra("Description"));
+    }
 }
