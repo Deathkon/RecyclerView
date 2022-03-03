90052a2fab6241f106cffdf7b037c830f540cb29
Deathkon <shyakasmith@gmail.com>
2022-03-03 08:56:15 +0200

    commit

--- app/src/main/java/com/example/recyclerview/ItemDetails.java
+++ app/src/main/java/com/example/recyclerview/ItemDetails.java
@@ -4,6 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class ItemDetails extends AppCompatActivity {
-
-    
+    ImageView imageView;
+    TextView name,price,tvDescription;
+     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_details);
+        imageView = findViewbyId(R.id.itemImageD);
+        name = findViewbyId(R.id.itemNameD);
+        price = findViewbyId(R.id.itemPriceD);
+        description = findViewbyId(R.id.itemDescrD);
+        
+        }
 }
